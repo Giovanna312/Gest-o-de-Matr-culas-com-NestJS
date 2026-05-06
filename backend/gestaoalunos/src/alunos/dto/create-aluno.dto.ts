e480d641c47b37729ca3965078eb7faa457df048
Gigi <giovanna_m_moraes@estudante.sesisenai.org.br>
2026-05-06 14:22:30 -0300

    67;0

--- backend/gestaoalunos/src/alunos/dto/create-aluno.dto.ts
+++ backend/gestaoalunos/src/alunos/dto/create-aluno.dto.ts
@@ -1,1 +1,19 @@
-export class CreateAlunoDto {}
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateAlunoDto {
+  @IsString()
+  @IsNotEmpty({ message: 'A matrícula não pode estar vazia' })
+  codigo_matricula: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'O nome não pode estar vazio' })
+  nome_completo: string;
+
+  @IsOptional()
+  @IsString()
+  situacao?: string;
+
+  @IsOptional()
+  @IsString()
+  acompanhamento?: string;
+}
